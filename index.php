7845ed9704e104ecfddb2095bdd5304edb0faf1e
Abhishek Maurya <janardanmaurya238@gmail.com>
2022-08-23 21:18:48 +0530

    Changed to php

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
     <meta name="description" content="An amazing portfolio generator">
     <meta name="author" content="Abhishek Maurya, Shashank Patil">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/0fe3b336ed.js" crossorigin="anonymous" media="screen"
-        integrity="sha384-4COO9rz8I0+qYYdRdIIoVHqpA+m9v9vPG1zpGeyVCs2MGAE7y1pflv6aTZ1o6u2C"></script>
+    <script src="https://kit.fontawesome.com/0fe3b336ed.js" crossorigin="anonymous" media="screen" integrity="sha384-4COO9rz8I0+qYYdRdIIoVHqpA+m9v9vPG1zpGeyVCs2MGAE7y1pflv6aTZ1o6u2C"></script>
     <link rel="stylesheet" href="css/base.css">
     <link rel="stylesheet" href="css/home.css">
     <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Sofia' media="screen">
@@ -32,10 +31,9 @@
                         <span></span>
                     </label>
                 </div>
-
                 <div class="nav-links">
                     <a href="https://github.com/NewbieCodes1/folio" target="_blank" rel="noopener">Github</a>
-                    <a href="" target="_blank">ABCDR</a>
+                    <a href="" target="_blank">ABCD</a>
                     <a mailto="mailto:shashankpatil360@gmail.com,">Contact Us</a>
                     <a href="" target="_blank">About Us</a>
                     <a href="login.php" target="_blank">Login</a>
