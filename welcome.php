b24f8c9ff3feaa60b5c2a4d4558b0abf782b6695
Abhishek Maurya <janardanmaurya238@gmail.com>
2022-09-01 16:30:27 +0530

    Added Pricing Page and Minor Fixes ⚒️

--- welcome.php
+++ welcome.php
@@ -1,5 +1,27 @@
-<html>
-    <head>
-        <title> Nice bhai </title>
-        </head>
-        </html> 
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> Folio </title>
+    <meta charset="utf-8">
+    <meta name="keywords" content="Folio, Portfolio, Portfolio Generator">
+    <meta name="description" content="An amazing portfolio generator">
+    <meta name="author" content="Abhishek Maurya, Shashank Patil">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/0fe3b336ed.js" media="screen"></script>
+    <link rel="stylesheet" href="components\css\base.css">
+    <link rel="stylesheet" href="components\css\nav.css">
+    <link rel="stylesheet" href="components\css\index.css">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Sofia' media="screen">
+</head>
+
+<body>
+    <div class="container">
+        <h1>This is welcome page</h1>
+    </div>
+</body>
+
+</html>
