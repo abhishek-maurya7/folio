7da8edaf98217badce1531bc5b7bf9fa10df1599
Abhishek Maurya <janardanmaurya238@gmail.com>
2022-09-16 12:07:30 +0530

    Form

--- components/classes/class.php
+++ components/classes/class.php
@@ -1,29 +1,33 @@
-<?php 
-    class Evaluate {
-        public function checkUsername($validateUsername) {
-            require 'components\db\_dbconnect.php';
-            $sql = "SELECT username FROM users WHERE username = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("s", $validateUsername);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            if (!$result->num_rows > 0) {
-                return true;
-            }
+<?php
+class Evaluate
+{
+    public function checkUsername($validateUsername)
+    {
+        require 'components\db\_dbconnect.php';
+        $sql = "SELECT username FROM users WHERE username = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $validateUsername);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if (!$result->num_rows > 0) {
             return false;
-        }
-        public function checkEmail($validateEmail) {
-            require 'components\db\_dbconnect.php';
-            $sql = "SELECT email FROM users WHERE email = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("s", $validateEmail);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            if (!$result->num_rows > 0) {
-                return true;
-            }
-            return false;
+        } else {
+            return true;
         }
     }
-    $evaluate = new Evaluate();
-?>
+    public function checkEmail($validateEmail)
+    {
+        require 'components\db\_dbconnect.php';
+        $sql = "SELECT email FROM users WHERE email = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $validateEmail);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if (!$result->num_rows > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+$evaluate = new Evaluate();
--- components/db/personalportfolio.sql
+++ components/db/personalportfolio.sql
@@ -18,46 +18,46 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `folio`
+-- Database: folio
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `personalportfolio`
+-- Table structure for table personalportfolio
 --
 
-CREATE TABLE `personalportfolio` (
-  `username` varchar(14) NOT NULL,
-  `firstName` varchar(12) NOT NULL,
-  `lastName` varchar(12) NOT NULL,
-  `email` varchar(35) NOT NULL,
-  `profileImg` mediumblob NOT NULL,
-  `aboutMe` varchar(250) NOT NULL,
-  `instagram` varchar(50) DEFAULT NULL,
-  `yt` varchar(50) DEFAULT NULL,
-  `github` varchar(60) DEFAULT NULL,
-  `twitter` varchar(20) DEFAULT NULL,
-  `facebook` varchar(20) DEFAULT NULL,
-  `LinkedIn` varchar(50) DEFAULT NULL,
-  `projectTitle` varchar(20) DEFAULT NULL,
-  `projectLink` varchar(20) DEFAULT NULL,
-  `projectDescription` varchar(250) DEFAULT NULL,
-  `projectTitle2` varchar(20) DEFAULT NULL,
-  `projectLink2` varchar(20) DEFAULT NULL,
-  `projectDescription2` varchar(250) DEFAULT NULL,
-  `projectTitle3` varchar(20) DEFAULT NULL,
-  `projectLink3` varchar(20) DEFAULT NULL,
-  `projectDescription3` varchar(250) DEFAULT NULL,
-  `certificateName` varchar(20) DEFAULT NULL,
-  `certificateClaimDate` date DEFAULT NULL,
-  `certificateLink` varchar(20) DEFAULT NULL,
-  `certificateName2` int(20) DEFAULT NULL,
-  `certificateClaimDate2` int(11) DEFAULT NULL,
-  `certificateLink2` int(20) DEFAULT NULL,
-  `certificateName3` int(20) DEFAULT NULL,
-  `certificateClaimDate3` int(11) DEFAULT NULL,
-  `certificateLink3` int(20) DEFAULT NULL,
+CREATE TABLE personalportfolio (
+  username varchar(14) NOT NULL,
+  firstName varchar(12) NOT NULL,
+  lastName varchar(12) NOT NULL,
+  email varchar(35) NOT NULL,
+  profileImg mediumblob NOT NULL,
+  aboutMe varchar(250) NOT NULL,
+  instagram varchar(50) DEFAULT NULL,
+  yt varchar(50) DEFAULT NULL,
+  github varchar(60) DEFAULT NULL,
+  twitter varchar(20) DEFAULT NULL,
+  facebook varchar(20) DEFAULT NULL,
+  LinkedIn varchar(50) DEFAULT NULL,
+  projectTitle varchar(20) DEFAULT NULL,
+  projectLink varchar(20) DEFAULT NULL,
+  projectDescription varchar(250) DEFAULT NULL,
+  projectTitle2 varchar(20) DEFAULT NULL,
+  projectLink2 varchar(20) DEFAULT NULL,
+  projectDescription2 varchar(250) DEFAULT NULL,
+  projectTitle3 varchar(20) DEFAULT NULL,
+  projectLink3 varchar(20) DEFAULT NULL,
+  projectDescription3 varchar(250) DEFAULT NULL,
+  certificateName varchar(20) DEFAULT NULL,
+  certificateClaimDate date DEFAULT NULL,
+  certificateLink varchar(20) DEFAULT NULL,
+  certificateName2 int(20) DEFAULT NULL,
+  certificateClaimDate2 int(11) DEFAULT NULL,
+  certificateLink2 int(20) DEFAULT NULL,
+  certificateName3 int(20) DEFAULT NULL,
+  certificateClaimDate3 int(11) DEFAULT NULL,
+  certificateLink3 int(20) DEFAULT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -65,20 +65,20 @@ CREATE TABLE `personalportfolio` (
 --
 
 --
--- Indexes for table `personalportfolio`
+-- Indexes for table personalportfolio
 --
-ALTER TABLE `personalportfolio`
-  ADD KEY `username` (`username`);
+ALTER TABLE personalportfolio
+  ADD KEY username (username);
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `personalportfolio`
+-- Constraints for table personalportfolio
 --
-ALTER TABLE `personalportfolio`
-  ADD CONSTRAINT `personalportfolio_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`);
+ALTER TABLE personalportfolio
+  ADD CONSTRAINT personalportfolio_ibfk_1 FOREIGN KEY (username) REFERENCES users (username);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
