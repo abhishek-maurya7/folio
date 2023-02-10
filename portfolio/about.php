d8566e895dbe100a8058275e6239933f80e098c2
Abhishek Maurya <janardanmaurya238@gmail.com>
2023-02-11 00:46:32 +0530

    Removed 2 code smells

--- portfolio/about.php
+++ portfolio/about.php
@@ -9,20 +9,6 @@
 
 <body>
     <h1>This is about of profile folder</h1>
-    <?php
-    $path = $_SERVER['REQUEST_URI'];
-    $explodes = explode('/', $path);
-    $explod = $explodes[2];
-    $file = basename($path);
-    echo $path;
-    echo '<br>';
-    echo '<pre>';
-    print_r($explodes);
-    echo '<br>';
-    echo $explod;
-    echo '<br>';
-    echo $file;
-    ?>
 </body>
 
 </html>
